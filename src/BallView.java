8881036560984f7693273f673d0a7527c3f91af5
Andrew Sheridan <mrajstech@gmail.com>
2014-11-24 13:56:49 -0500

    Swapped View and Controller Code

--- src/BallView.java
+++ src/BallView.java
@@ -1,139 +1,212 @@
+import java.applet.Applet;
 import java.applet.AudioClip;
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Event;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.Image;
+
+//hi
 
 
-public class BallView {
-	private int pos_x;
-	private int pos_y;
-	private int x_speed;
-	private int y_speed;
-	private int radius;
+public class BallView extends Applet implements Runnable{
 	
-	private int first_x;
-	private int first_y;
+	//Declare variables
+	private int speed;
 	
-	private int maxspeed;
+	boolean isStopped = true;
 	
-	private final int x_leftout = 10;
-	private final int x_rightout = 370;
-	private final int y_upout = 45;
-	private final int y_downout = 370;
+	//Declare objects
+	private BallModel player;
+	private BallController redball;
+	private BallController blueball;	
 	
-	Color color;
+	Thread th;
 	
-	AudioClip out;
+	//Audio Clips
+	AudioClip shotnoise;
+	AudioClip hitnoise;
+	AudioClip outnoise;
 	
-	BallModel player;
+	//new Font
+	Font f = new Font("Serif", Font.BOLD, 20);
 	
-	Random rnd = new Random();
+	//Set up cursor
+	Cursor c;	//cursor variable	
 	
-	public BallView (int radius, int x, int y, int vx, int vy, int ms, Color color, AudioClip out, BallModel player)
-	{
-		this.radius = radius;
-		pos_x = x;
-		pos_y = y;
-		first_x = x;
-		first_y = y;
-		x_speed = vx;
-		y_speed = vy;
-		maxspeed = ms;
-		this.color = color;
-		this.out = out;
-		this.player = player;
+	private Image dbImage;
+	private Graphics dbg;
+	
+	public void init(){
+		setSize(400,400);
+		c = new Cursor (Cursor.CROSSHAIR_CURSOR);
+		this.setCursor(c);
+		
+		Color superblue = new Color (0, 0, 255);
+		
+		setBackground(Color.black);
+		
+		setFont(f);
+		
+		//set speed of applet
+		
+		if (getParameter ("speed") != null)
+		{
+			speed = Integer.parseInt(getParameter("speed"));
+		}
+		else speed = 15;
+		
+		//preload audio files
+		
+		hitnoise = getAudioClip (getCodeBase(), "gun.au");
+		hitnoise.play();
+		hitnoise.stop();
+		shotnoise = getAudioClip (getCodeBase(), "miss.au");
+		shotnoise.play();
+		shotnoise.stop();
+		outnoise = getAudioClip (getCodeBase(), "error.au");
+		outnoise.play();
+		outnoise.stop();
+		
+		//initialize game objects
+		player = new BallModel();
+		redball = new BallController (10, 190, 250, 1, -1, 4, Color.red, outnoise, player);
+		blueball = new BallController (10, 190, 150, 1, 1, 3, Color.blue, outnoise, player);
+		
+					
+	}
+	
+	public void start(){
+		th = new Thread(this);
+		th.start();
+	}
+	
+	public void stop(){
+		th.stop();
+	}
+	
+	public boolean mouseDown(Event e, int x, int y){
+		
+		if (!isStopped)
+		{
+			if (redball.userHit(x,y))
+			{
+				hitnoise.play();
+				redball.ballWasHit();
+			}
+			if (blueball.userHit(x,y))
+			{
+				hitnoise.play();
+				blueball.ballWasHit();
+			}
+			else
+			{
+				shotnoise.play();
+			}
+		}
+		
+		else if (isStopped && e.clickCount == 2)
+		{
+			isStopped = false;
+			init();
+		}
+		
+		return true;
+	}
+	
+	public void run(){
+		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+		
+		while (true)
+		{
+			
+			if (player.getLives() >= 0 && !isStopped)
+			{
+				redball.move();
+				blueball.move();
+			}
+			
+			repaint();
+			
+			try
+			{
+				Thread.sleep (speed);
+			}
+			catch (InterruptedException ex)
+			{
+				//do nothing
+			}
+			
+		Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+		}
+	}
+	
+	public void paint (Graphics g){
+		//if lives still remain
+		if (player.getLives() >=0)
+		{	//set the background color
+			g.setColor(Color.yellow);
+			//display standings
+			g.drawString("Score: " + player.getScore(), 10, 40);
+			g.drawString("Lives: " + player.getLives(), 300, 40);
+			
+			//draw the balls
+			redball.drawBall(g);
+			blueball.drawBall(g);
+			
+			if (isStopped)
+			{
+				g.setColor (Color.yellow);
+				g.drawString("Double-click on Applet to start game.", 40, 200);
+			}
+		}
+		
+		else if (player.getLives() < 0)
+		{
+			g.setColor(Color.yellow);
+			
+			//points and game over
+			g.drawString("Game Over!", 130, 100);
+			g.drawString("You scored " + player.getScore() + " Points!", 90, 140);
+			
+			//Assessment of points
+			if (player.getScore() < 300) g.drawString ("You got no swag", 100, 190);
+			else if (player.getScore() < 600 && player.getScore() >= 300 ) 
+				g.drawString ("Do better! No swag", 100, 190);
+			else if (player.getScore() < 900 && player.getScore() >= 600 ) 
+				g.drawString ("Almost swag", 100, 190);
+			else if (player.getScore() < 1200 && player.getScore() >= 900 ) 
+				g.drawString ("Beast status", 100, 190);
+			else if (player.getScore() < 1500 && player.getScore() >= 1200 ) 
+				g.drawString ("Champ status achieved", 100, 190);
+			else if (player.getScore() >= 1500) 
+				g.drawString ("Major swag status achieved", 100, 190);
+			g.drawString ("Double-click on the Applet to play again", 20, 220);
+			isStopped = true;
+		}
+		
+		
+		
 		
 	}
 	
-	public void move()
-	{
-		pos_x += x_speed;
-		pos_y += y_speed;
-		isOut(); //method to test if the ball is out of bounds
-	}
-	
-	public void ballWasHit()
-	{
-		pos_x = first_x;
-		pos_y = first_y;
+	public void update (Graphics g){
 		
-		x_speed = (rnd.nextInt()) % maxspeed;
-	}
-	
-	public boolean userHit(int mouse_x, int mouse_y)
-	{
-		double x = mouse_x - pos_x;
-		double y = mouse_y - pos_y;
-		double distance = Math.sqrt((x*x) + (y*y));
+		if (dbImage == null)
+		{
+			dbImage = createImage (this.getSize().width, this.getSize().height);
+			dbg = dbImage.getGraphics();
+		}
 		
-		if (distance < 15)
-		{
-			player.addScore(10*Math.abs(x_speed)+10);
-			return true;
-		}
-		else return false;
+		dbg.setColor (getBackground());
+		dbg.fillRect (0, 0, this.getSize().width, this.getSize().height);
+		
+		dbg.setColor (getForeground());
+		paint (dbg);
+		
+		g.drawImage (dbImage, 0, 0, this);
 	}
 	
-	private boolean isOut()
-	{
-		if (pos_x < x_leftout)
-		{
-			pos_x = first_x;
-			pos_y = first_y;
-			
-			out.play();
-			
-			x_speed = (rnd.nextInt()) % maxspeed;
-			
-			player.loseLife();
-			
-			return true;
-		} 
-		else if (pos_x > x_rightout)
-		{
-			pos_x = first_x;
-			pos_y = first_y;
-			
-			out.play();
-			
-			x_speed = (rnd.nextInt()) % maxspeed;
-			
-			player.loseLife();
-			
-			return true;
-		}
-		else if (pos_y < y_upout)
-		{
-			pos_x = first_x;
-			pos_y = first_y;
-			
-			out.play();
-			
-			x_speed = (rnd.nextInt()) % maxspeed;
-			
-			player.loseLife();
-			
-			return true;
-		}
-		else if (pos_y > y_downout)
-		{
-			pos_x = first_x;
-			pos_y = first_y;
-			
-			out.play();
-			
-			x_speed = (rnd.nextInt()) % maxspeed;
-			
-			player.loseLife();
-			
-			return true;
-		}
-		else return false;
-	}
 	
-	public void drawBall(Graphics g)
-	{
-		g.setColor(color);
-		g.fillOval(pos_x - radius, pos_y - radius, 2*radius, 2*radius);
-	}
 }
